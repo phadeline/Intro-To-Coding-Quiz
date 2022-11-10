0ad380b67b25dc9885e41616d4c35dd292e4f635
phadeline <112015433+phadeline@users.noreply.github.com>
2022-11-09 20:18:49 -0500

    added styling to the elements

--- develop/newscript.js
+++ develop/newscript.js
@@ -42,25 +42,52 @@ let length = questions.length;
 let index = 0;
 
 let body = document.body;
+// kittenEl.setAttribute("style", "font-size:25px; text-align:center;")
+body.setAttribute(
+  "style",
+  "max-width: 100%; max-height: 100%; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;"
+);
 
 //creates the text explaining the rules of the quiz
 const elementh1 = document.createElement("h1");
 const elementh2 = document.createElement("h2");
 elementh1.textContent = "Welcome to Rutgers Bootcamp Coding Quiz!!";
 elementh2.textContent =
-  "Here are the rules: Choose the best answer for each question. IF you pick a wrong answer your time will decrease.";
+  "Here are the rules: Choose the best answer for each question. IF you pick a wrong answer your time will DECREASE.";
 
 body.appendChild(elementh1);
 body.appendChild(elementh2);
 
+//div element that will contain the start button
+let box = document.createElement("div");
+let boxcontainer = box.setAttribute("id", "box");
+box.setAttribute("style", "position:relative; top: 150px");
+body.appendChild(box);
+
+//This is the button the user will press to start the game.
+let btn = document.createElement("button");
+let btnelement = btn.setAttribute("id", "button");
+btn.textContent = "Click Me To Start";
+btn.setAttribute(
+  "style",
+  "background-color: aqua; padding: 50px; font-size: 30px"
+);
+
+box.appendChild(btn);
+
 //created an element that hold the countdown timer
 let time = document.createElement("p");
+time.setAttribute("style", "font-size:30px; color: red;");
 let timer = body.appendChild(time);
 timer.setAttribute("id", "timer");
 
 //creates div container which will have the questions and answers as its children
 const container = document.createElement("div");
 container.setAttribute("id", "qacontainer");
+container.setAttribute(
+  "style",
+  "display: flex; flex-direction: column; justify-content: center; align-items: center;"
+);
 body.appendChild(container);
 
 //This function creates the container for the user to be able to enter their name and score.
@@ -96,25 +123,29 @@ function SaveName() {
   buttonname.setAttribute("style", "width:50px; height: 20px;");
   buttonname.textContent = "Add!";
 
-//
-  var info = JSON.parse(localStorage.getItem("names")) || [];
+  //info will contain all the user info (names and score).
+  var info = [];
+  let infos = JSON.parse(localStorage.getItem("names"));
+
+  //this is done so that info won't be an empty every time a user puts add their information.
+  info = infos;
 
   //add eventlister to the button so that user can submit their name
   pressme.addEventListener("click", function (event) {
     event.stopPropagation();
     event.preventDefault();
 
-    //EVERY PLAYER NAME WILL BE PUT INTO AN ARRAY AFTER THEY CLICK THE BUTTON
+    //player is the input of what the user writes for their name
     let player = document.getElementById("username").value;
 
+    //the player's name and their score will be added to the variable info
     info.push({
       playername: player,
       scores: score,
     });
 
+    //the user's name and score is added to local storage
     localStorage.setItem("names", JSON.stringify(info));
-
-    //need to set score and player name into local storage
 
     if (player === "") {
       alert("Please enter a name in the input box");
@@ -144,21 +175,43 @@ function displayQuestion(questions, index) {
   //this creates the element "ul" and container of the answers to pick from
   const unorderedList = document.createElement("ul");
   unorderedList.setAttribute("id", "qContainer");
+  unorderedList.setAttribute(
+    "style",
+    "width: 600px; list-style: none; text-align: center; padding: 20px; display: flex; flex-direction: column; align-items: center; justify-content: center;"
+  );
 
   //this creates the "li" elements which will hold the answers
   const li1 = document.createElement("li");
   const li2 = document.createElement("li");
   const li3 = document.createElement("li");
   const li4 = document.createElement("li");
+  const allist = document.querySelectorAll("li");
 
   //this creates the "h3" element which will hold the questions
   const h3 = document.createElement("h3");
+  h3.setAttribute("style", "font-size: 35px; color: navy;");
 
   //this assigns "id" to each "li" elements
   li1.setAttribute("id", "li1");
+  li1.setAttribute(
+    "style",
+    "width: 100%; padding: 10px; font-size: 25px; border-style: solid; margin: 5px; border-radius: 20px; background-color: aqua;"
+  );
   li2.setAttribute("id", "li2");
+  li2.setAttribute(
+    "style",
+    "width: 100%; padding: 10px; font-size: 25px; border-style: solid; margin: 5px; border-radius: 20px; background-color: aqua;"
+  );
   li3.setAttribute("id", "li3");
+  li3.setAttribute(
+    "style",
+    "width: 100%; padding: 10px; font-size: 25px; border-style: solid; margin: 5px; border-radius: 20px; background-color: aqua;"
+  );
   li4.setAttribute("id", "li4");
+  li4.setAttribute(
+    "style",
+    "width: 100%; padding: 10px; font-size: 25px; border-style: solid; margin: 5px; border-radius: 20px; background-color: aqua;"
+  );
 
   qacontainer.appendChild(h3);
   qacontainer.appendChild(unorderedList);
@@ -198,15 +251,13 @@ function displayQuestion(questions, index) {
   });
 }
 
-let btn = document.getElementById("button");
-
 btn.addEventListener("click", function (event) {
   displayQuestion(questions, 0);
   btn.setAttribute("style", "display:none");
   var Countdown = setInterval(function () {
     if (count > 0) {
       count--;
-      timer.textContent = count;
+      timer.textContent = " Time: " + count;
     } else {
       timer.textContent = "Game Over";
       clearInterval(Countdown);
